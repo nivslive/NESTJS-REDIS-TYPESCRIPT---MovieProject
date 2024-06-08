f3110f450410f0d7853df4ac45ac7eb575bbf26c
nivs <nivsoficial@gmail.com>
2024-06-08 12:13:17 -0300

    FEAT: config db on app root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'moviesdb',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    AuthModule,
+    MoviesModule,
+  ],
 })
 export class AppModule {}
